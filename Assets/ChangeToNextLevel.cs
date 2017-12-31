253ad8621eefb90e04bc4735163c4dbba0121ab5
AlanCesarAraujo <alancesar389@gmail.com>
2017-12-31 16:13:34 -0300

    Fases do Dó (Sem transições)

---
+++ Assets/ChangeToNextLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeToNextLevel : MonoBehaviour {
+
+	public GameObject nextUp;
+
+	public void goToNextLevel(){
+		nextUp.SetActive(false);
+	}
+}
--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -9,16 +9,14 @@ public class ChangeScene : MonoBehaviour {
 	public int index;
 	public GameObject fase;
 	public GameObject buttons1;
-	public GameObject buttons0;
 	public GameObject buttons;
 	public GameObject victory;
 
 	public void goToScene(){
+		victory.SetActive(false);
 		fase.SetActive(true);
 		buttons1.SetActive(true);
 		buttons.SetActive(false);
-//		buttons0.SetActive(true);
-		victory.SetActive(false);
 
 		Debug.Log("Index: "+ index +" deu certo");
 	}
